c06de68bef23f0ca9ec251442dc2c6d746439645
Heaven GolladayWatkins <heaven.golladaywatkins@reddit.com>
2024-08-06 14:06:04 -0400

    Co-authored-by: UnluckyHuckleberry53 <UnluckyHuckleberry53@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import {
   FormValues,
   User,
   FormOnSubmitEvent,
+  FormKey
 } from "@devvit/public-api";
 import { usePagination } from "@devvit/kit";
 import { RedditAPIClient } from "@devvit/public-api";
@@ -23,14 +24,122 @@ type Meet = {
   attending: string[];
 };
 
+//function returning true/false if user is in event.attending
+const userAttendingEvent = ({
+  currentUsername,
+  attendingUsersList,
+  eventTitle,
+}: {
+  currentUsername: string | undefined;
+  attendingUsersList: string[];
+  eventTitle: string;
+}) => { 
+  console.log("----------------")
+  console.log("event title:", eventTitle);
+  console.log("attending users list", attendingUsersList);
+  if (currentUsername) {
+    return attendingUsersList.includes(currentUsername);
+  } else {
+    console.log("User not detected");
+    return false;
+  }
+};
+
+const RSVP_NOTIF = "reminder";
+
+Devvit.addSchedulerJob({
+  name: RSVP_NOTIF,
+  onRun: async (event, context) => {
+    const data = event.data!.data as Meet;
+
+    // Iterate over the 'attending' array
+    for (const username of data.attending) {
+      await context.reddit.sendPrivateMessage({
+        to: username, 
+        subject: "Event Reminder",
+        text: `Hello! ${data.title} is starting soon! ${data.link}`,
+      });
+    }
+  },
+});
+
+//formatting of how event items displayed onto app
+const ItemComponent: Devvit.BlockComponent<{
+  item: Meet;
+  onDeleteItem: (item: Meet) => void;
+  onRSVP: (item: Meet) => void;
+  currentUsername: string | undefined;
+  infoFormKey: FormKey
+}> = (props, context): JSX.Element => {
+  const canUserAttend = userAttendingEvent({
+    attendingUsersList: props.item.attending,
+    eventTitle: props.item.title,
+    currentUsername: props.currentUsername,
+  })
+
+
+
+  return (
+    <vstack>
+      <hstack>
+        <vstack>
+          <hstack gap="small">
+            <text size="xxlarge" weight="bold">
+              {props.item.title}
+            </text>
+            <button
+              size="small"
+              icon="info"
+              onPress={() => context.ui.showForm(props.infoFormKey, props.item)}
+            />
+            <button
+              size="small"
+              icon="delete"
+              onPress={() => props.onDeleteItem(props.item)}
+            />
+          </hstack>
+          <vstack>
+            <text weight="bold">{props.item.date}</text>
+          </vstack>
+        </vstack>
+
+        <hstack alignment="end middle" grow gap="small">
+          <text alignment="end" size="xxlarge">
+            {props.item.startTime}
+          </text>
+          <button
+            icon={canUserAttend ? "notification-frequent-fill" : "notification"}
+            size="small"
+            onPress={() => props.onRSVP(props.item)}
+          />
+        </hstack>
+      </hstack>
+      <vstack backgroundColor="#F1F1F1" height="2px"></vstack>
+    </vstack>
+  );
+};
+
 Devvit.addCustomPostType({
   name: "Name",
-  render: async (context) => {
-    const { reddit, ui } = context;
-    const currentSubreddit = await reddit.getCurrentSubreddit();
+  render: (context) => {
+    const {reddit} = context;
+    const [currentUsername] = context.useState(async () => {
+      const currentUser = await reddit.getCurrentUser();
+      return currentUser?.username;
+    });
+
+    
+    const [currentSubreddit] = context.useState(async () => {
+      const currentSubreddit = await reddit.getCurrentSubreddit();
+      
+      return {
+        name: currentSubreddit.name,
+      };
+    });
+    
 
     //function to rerender events when list of events is updated
-    let [localEvents, setLocalEvents] = context.useState<Meet[]>(async () => {
+    const [localEvents, setLocalEvents] = context.useState<Meet[]>(async () => {
       const eventsStr = await context.redis.get("eventapp");
 
       if (!eventsStr) {
@@ -40,8 +149,22 @@ Devvit.addCustomPostType({
       return JSON.parse(eventsStr);
     });
 
-    //onSubmit function for info form
-    const dummySubmit = () => {};
+
+    //function to delete event from events board
+    const deleteEvent = async (data: any) => {
+      console.log(data);
+      console.log(localEvents);
+
+      const newLocalEvents: Meet[] = localEvents.filter(
+        (event) => event !== data
+      );
+      await context.redis.set("eventapp", JSON.stringify(newLocalEvents));
+
+      setLocalEvents(newLocalEvents);
+
+      console.log(newLocalEvents);
+      context.ui.showToast("Event deleted!");
+    };
 
     //when add event form is submitted update upcomingEvents
     const onSubmit = async (values: any) => {
@@ -61,209 +184,95 @@ Devvit.addCustomPostType({
       //update redis data
       await context.redis.set("eventapp", JSON.stringify(newLocalEvents));
 
-      //call useState hook
       setLocalEvents(newLocalEvents);
 
       context.ui.showToast("Event created!");
+
+      remindUser(event, context);
     };
 
-    //function returning true/false if user in event.attending
-    const userAttendingEvent = async (
-      attendingUsersList: string[],
-      eventTitle: string
-    ) => {
-      const person = await reddit.getCurrentUser();
-      console.log(
-        "user attending event attending users list",
-        attendingUsersList
-      );
-      console.log("event title", eventTitle);
-      if (person) {
-        const user = person.username;
-        const usertag = JSON.stringify(user);
-        return await attendingUsersList.includes(usertag);
-      } else {
-        console.log("User not detected");
-        return false;
-      }
-    };
+    /** function to add users to an event's attending list */
+    const RSVP = async (_event: Meet) => {
+      const event = { ..._event };
 
-    //function to add users to an event's attending list
-    const RSVP = async (event: Meet) => {
-      const isUserAttending = await userAttendingEvent(
-        event.attending,
-        event.title
-      );
-      const person = await reddit.getCurrentUser();
-      console.log("RSVP event", event);
-      if (person) {
-        const user = person.username;
-        const usertag = JSON.stringify(user);
-        if (usertag) {
+      const isUserAttending = userAttendingEvent({
+        currentUsername,
+        attendingUsersList: event.attending,
+        eventTitle: event.title,
+      });
+  
+        if (currentUsername) {
           //if the user is already attending event, and the function is called, unRSVP
           if (isUserAttending) {
             const newAttending = event.attending.filter(
-              (users) => users !== usertag
-            );
+              (users) => users !== currentUsername );
             event.attending = newAttending;
             console.log(
               "current user attending event: expecting false",
-              await userAttendingEvent(newAttending, event.title)
+              userAttendingEvent({
+                currentUsername,
+                attendingUsersList: event.attending,
+                eventTitle: event.title,
+              })
             );
             const newLocalEvents: Meet[] = [...localEvents];
             await context.redis.set("eventapp", JSON.stringify(newLocalEvents));
-            localEvents = newLocalEvents;
+
+            setLocalEvents(newLocalEvents);
           }
 
           //if the user is not attending the event, add them to the attending list
           else {
-            event.attending.push(usertag);
-            console.log(
-              "current user attending event: expecting true",
-              await userAttendingEvent(event.attending, event.title)
-            );
+            event.attending.push(currentUsername);
             const newLocalEvents: Meet[] = [...localEvents];
             await context.redis.set("eventapp", JSON.stringify(newLocalEvents));
-            localEvents = newLocalEvents;
+            setLocalEvents(newLocalEvents);
+            console.log(
+              "current user attending event: expecting true",
+              userAttendingEvent({
+                currentUsername,
+                attendingUsersList: event.attending,
+                eventTitle: event.title,
+              })
+            );
+            
           }
+        
+      }    };
+
+      async function remindUser(event: Meet, context: Devvit.Context) {
+        const whenStr = `15 minutes prior to ${event.startTime} ${event.date}`|| '';
+        if (!whenStr) {
+          context.ui.showToast("Error: Invalid time format");
+          return;
         }
+
+        const parsedTime = chrono.parseDate(whenStr);
+        const now = new Date();
+
+        if (parsedTime)
+        {if (parsedTime < now) {
+          context.ui.showToast("Error: Invalid time format");
+          return;
+        }
+          await context.scheduler.runJob({
+            name: RSVP_NOTIF,
+            data: {
+              data: event,
+            },
+            runAt: parsedTime,
+          });
+        
+
+        context.ui.showToast(`Gotcha! I'll send you a message about this post at ${parsedTime}!`);
       }
-      console.log("users attending event", event.attending);
-    };
-
-    // const RSVP_NOTIF = 'reminder';
-
-    //   async function remindUser(event: Meet, context: Devvit.Context) {
-    //     const whenStr = `10 minutes prior to ${event.startTime} ${event.date}`|| '';
-    //     if (!whenStr) {
-    //       context.ui.showToast("I don't know when to remind you!");
-    //       return;
-    //     }
-
-    //     const parsedTime = chrono.parseDate(whenStr);
-    //     const now = new Date();
-
-    //     if (parsedTime)
-    //     {if (parsedTime < now) {
-    //       context.ui.showToast("I can't remind you in the past!");
-    //       return;
-    //     }}
-    //     const currentUser = await context.reddit.getCurrentUser();
-
-    //     if (currentUser != null && parsedTime != null) {
-    //       await context.scheduler.runJob({
-    //         name: RSVP_NOTIF,
-    //         data: {
-    //           userId: currentUser.id,
-    //           postId: context.postId,
-    //           fromWhen: now,
-    //         },
-    //         runAt: parsedTime,
-    //       });
-    //     }
-
-    //     context.ui.showToast(`Gotcha! I'll send you a message about this post at ${parsedTime}!`);
-    //   }
-
-    //   Devvit.addSchedulerJob({
-    //     name: RSVP_NOTIF,
-    //     onRun: async (event, context) => {
-    //       const {userId} = event.data!;
-
-    //       const user = await context.reddit.getUserById(userId);
-    //       
-
-    //       await context.reddit.sendPrivateMessage({
-    //         to: user.username,
-    //         subject: 'RemindMe',
-    //         text: `Hello! ${event.title} is starting soon! ${event.link}`,
-    //       });
-    //     },
-    //   });
-
-    //function to delete event from events board
-    const deleteEvent = async (data: any) => {
-      console.log(data);
-      console.log(localEvents);
-
-      const newLocalEvents: Meet[] = localEvents.filter(
-        (event) => event !== data
-      );
-      await context.redis.set("eventapp", JSON.stringify(newLocalEvents));
-
-      setLocalEvents(newLocalEvents);
-
-      console.log(newLocalEvents);
-      context.ui.showToast("Event deleted!");
-    };
-
+}
     //pagination variables, localEvents = currentItems = list of events displayed in app
     const { currentPage, currentItems, toNextPage, toPrevPage } = usePagination(
       context,
       localEvents,
       3
     );
-
-    //formatting of how event items displayed onto app
-    const ItemComponent = async (props: {
-      item: Meet;
-    }): Promise<JSX.SyncElement> => {
-      return (
-        <vstack>
-          <hstack>
-            <vstack>
-              <hstack gap="small">
-                <text size="xxlarge" weight="bold">
-                  {props.item.title}
-                </text>
-                <button
-                  size="small"
-                  icon="info"
-                  onPress={() => context.ui.showForm(infoForm, props.item)}
-                />
-                <button
-                  size="small"
-                  icon="delete"
-                  onPress={() => deleteEvent(props.item)}
-                />
-              </hstack>
-              <vstack>
-                <text weight="bold">{props.item.date}</text>
-              </vstack>
-            </vstack>
-
-            <hstack alignment="end middle" grow gap="small">
-              <text alignment="end" size="xxlarge">
-                {props.item.startTime}
-              </text>
-              <button
-                icon={
-                  (await userAttendingEvent(props.item.attending, props.item.title))  ? "notification-frequent-fill" : "notification"}
-                size="small"
-                onPress={() => RSVP(props.item)}
-              />{" "}
-            </hstack>
-          </hstack>
-          <vstack backgroundColor="#F1F1F1" height="2px"></vstack>
-        </vstack>
-      );
-    };
-    
-    //form that displays description and event time
-    const infoForm = context.useForm((data) => {
-      return {
-        fields: [
-          {
-            label: `${data.startTime}-${data.endTime}`,
-            type: "paragraph",
-            name: "Description",
-            defaultValue: data.description,
-            disabled: true,
-          },
-        ],
-        cancelLabel: "Back",
-      };
-    }, dummySubmit);
 
     //form to create event
     const eventForm = context.useForm(
@@ -315,19 +324,30 @@ Devvit.addCustomPostType({
       onSubmit
     );
 
+      //onSubmit function for info form
+  const dummySubmit = () => {};
+
+  //form that displays description and event time
+  const infoForm = context.useForm((data) => {
+    return {
+      fields: [
+        {
+          label: `${data.startTime}-${data.endTime}`,
+          type: "paragraph",
+          name: "Description",
+          defaultValue: data.description,
+          disabled: true,
+        },
+      ],
+      cancelLabel: "Back",
+    };
+  }, dummySubmit);
+
     // Render the appp
     return (
       <vstack>
         <vstack width="100%" height="64px" backgroundColor="#003285">
           <hstack padding="small" alignment="middle">
-            <blocks>
-              <image
-                imageHeight={50}
-                imageWidth={50}
-                //i.reddit.com
-                url="https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Fphoto-to-test-things-v0-0ffb0mzzvq7d1.png%3Fauto%3Dwebp%26s%3D2bb151094491773b27963ffa8befeff8693f859a"
-              />
-            </blocks>
             <text color="white" size="xxlarge" weight="bold">
               r/{currentSubreddit.name}
             </text>
@@ -337,8 +357,7 @@ Devvit.addCustomPostType({
                 size="medium"
                 onPress={() => context.ui.showForm(eventForm)}
               >
-                {" "}
-                Add Event{" "}
+                Add Event
               </button>
             </zstack>
           </hstack>
@@ -347,7 +366,13 @@ Devvit.addCustomPostType({
         {/* Rendering items for the current page */}
         <vstack gap="small" padding="small" minHeight="200px">
           {currentItems.map((item) => (
-            <ItemComponent item={item} />
+            <ItemComponent
+              item={item}
+              onDeleteItem={deleteEvent}
+              onRSVP={RSVP}
+              currentUsername={currentUsername}
+              infoFormKey={infoForm}
+            />
           ))}
         </vstack>
 
